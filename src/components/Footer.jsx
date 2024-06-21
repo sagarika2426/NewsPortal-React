acc958a5dcd7b13b81706d1db9708fa4c54370e9
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-06-21 21:32:33 +0530

    footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <div className="bg-sky-200 text-center py-8">
+            <p className="font-semibold m-auto text-center my-0"> Developed by Sagarika Sahoo</p>
+            <a href="https://www.linkedin.com/in/sagarika2412/"
+            className="text-center text-red-800 font-semibold my-0">Connect</a>
+        </div>
+    )
+}
+
+export default Footer;
