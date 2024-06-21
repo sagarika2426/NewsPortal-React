c92cb28254c51eac9f0e1fffcfa5234a5d8e0120
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-06-21 17:20:32 +0530

    changed pagination according to search

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,8 +2,14 @@ import { Pagination } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 const PaginationBar = ({ onPageChange }) => {
-  const { articles, currentPage, articlesPerPage } = useSelector((state) => state.articles);
-  const totalArticles = articles.length;
+  const { articles, currentPage, articlesPerPage, searchQuery } = useSelector((state) => state.articles);
+
+  // Filter articles based on search query
+  const filteredArticles = articles.filter((article) =>
+    article.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const totalArticles = filteredArticles.length;
 
   return (
     <div className="flex justify-center mt-8">
