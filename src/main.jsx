47bbeb4b7815a4c565e1e1ffc66789a5b77cd5de
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-06-21 21:32:15 +0530

    added footer

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import NavBar from "./components/navbar.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <NavBar />
       <App />
+      <Footer/>
     </BrowserRouter>
   </Provider>
 );
